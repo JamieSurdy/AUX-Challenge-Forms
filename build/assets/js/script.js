368ba9692d2e57c83e7889c0d2f785e9b7bb7881
JamieSurdy <jamie.surdykowski@gmail.com>
2015-03-10 16:43:07 -0400

    3/10

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -1,1 +1,44 @@
 /* Your JavaScript goes here */
+
+
+// //ccDefinitions = {
+// 	'.sprite_visa': /^4/,
+// 	'.sprite_mc': /^5[1-5]/,
+// 	'.sprite_amex': /^3(4|7)/,
+// 	'.sprite_disc': /^6011/
+// };
+
+
+// getCreditCardType: function (number) {
+
+// 	var ccType;
+
+// 	$.each(ccDefinitions, function (i, v) {
+
+// 		if (v.test(number)) {
+// 			ccType = i;
+// 			return false;
+// 		}
+
+// 	});
+
+// 	return ccType;
+
+// },//
+
+// if (condition)
+// 	statement1
+// [else
+// 	statement2]
+
+if $("#card_number").val("37"){
+	statement1
+
+} else if $("#card_number").val("4"){
+	statement2
+
+} else if $("#card_number").val("5")
+	statement3
+
+} else $("#card_number").val("6"){
+	statement4}

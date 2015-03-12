349f9ce51c26aa9c81b8fe1fd45c5a9063884fc2
JamieSurdy <jamie.surdykowski@gmail.com>
2015-03-12 17:01:31 -0400

    3/12 v2

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -1,11 +1,12 @@
 /* Your JavaScript goes here */
 
-	(function work(){
+	$(function work(){
 
 		var amex=$('#cardtype_1');
 		var visa=$('#cardtype_2');
 		var mc=$('#cardtype_4');
 		var discover=$('#cardtype_3')
+		var secCode = $('#security_code')
 
 		//if selected, this value shows up
 		$.fn.checked = function(value){
@@ -14,15 +15,14 @@
 				$(this).each(function(){this.checked=value; });
 			}
 		}
-	});
+	};
+	
+		function getCardNumber(){
+			$('cardNumber').keyup(function(){
+				var card = $(this).val();
+				var cardNumber = (card).slice(0,1);
 
-
-	function getCardNumber(){
-		$('#card_number').keyup(function(){
-			var card = $(this).val();
-			var cardNumber =(card).slice(0,1);
-
-			console.log(cardNumber);
+				console.log(cardNumber);
 
 			if (cardNumber === '37'){
 				(amex).checked(true);
@@ -34,14 +34,44 @@
 				(discover).checked(true);
 			}
 
+
 		});
-};
+
+	};
+
+	(amex).change(function()){
+		if ($(this).prop('checked')){
+			secCode.addClass('amex');
+		} else{
+			secCode.removeClass('amex')
+		}
+	});
+
 
 	getCardNumber();
 	
+	Modernizr.load({
+			test: Modernizr.input.required,
+			yep: console.log('required works here!'),
+			nope: ['vendor/jquery.validate.min.js'],
+
+			// call jQuery Validate if required is not supported
+			complete: function () {
+				if (!Modernizr.input.required) {
+				$('form').each(function() {
+				$(this).validate();
+
+			});
+
+		}
+
+	}
+
 });
 
-work();
+})();
+
+$.work()};
 
 
 
